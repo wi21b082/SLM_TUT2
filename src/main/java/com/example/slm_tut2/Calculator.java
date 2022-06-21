de512e76767a7c81839e3373a29fbf897534a63c
wi21b082 <can.oezalp@gmx.at>
2022-06-21 19:38:12 +0200

    Initial commit

---
+++ src/main/java/com/example/slm_tut2/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.slm_tut2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator.class, args);
+    }
+
+}
